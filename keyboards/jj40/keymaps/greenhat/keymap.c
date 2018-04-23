4ffdd51c859aba016518580392796470c47b8027
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2018-04-23 20:00:10 +0300

    Finalizes modifier row;

--- keyboards/jj40/keymaps/greenhat/keymap.c
+++ keyboards/jj40/keymaps/greenhat/keymap.c
@@ -20,14 +20,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Esc  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Ctrl | Alt  | GUI  |Lower | Space(Shift)|Raise | Left | Down |  Up  |Right |
+ * |      | Lower|  Alt | GUI  | Raise| Space(Shift)|Raise | GUI  |  Alt | Lower|      |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = KEYMAP( \
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,             KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT, \
-  KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,             KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RCTL, \
-  KC_ESC,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,             KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-  _______, KC_LCTL, KC_LALT, KC_LGUI, MO(_LOWER),  MT(MOD_LSFT, KC_SPC),  MO(_RAISE),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,       KC_T,    KC_Y,         KC_U,       KC_I,    KC_O,    KC_P,      KC_QUOT, \
+  KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,       KC_G,    KC_H,         KC_J,       KC_K,    KC_L,    KC_SCLN,   KC_RCTL, \
+  KC_ESC,     KC_Z,    KC_X,    KC_C,    KC_V,       KC_B,    KC_N,         KC_M,       KC_COMM, KC_DOT,  KC_SLSH,   KC_ENT , \
+  _______, MO(_LOWER), KC_LALT, KC_LGUI, MO(_RAISE), MT(MOD_LSFT, KC_SPC),  MO(_RAISE), KC_RGUI, KC_RALT, MO(_LOWER), _______ \
 ),
 
 /* Lower
@@ -56,14 +56,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |      |      |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      | Bksp(Shift) |      | Next | Vol- | Vol+ | Play |
+ * |      |      |      |      |      | Bksp(Shift) |      |      |      | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = KEYMAP( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,          KC_7,    KC_8,    KC_9,    KC_0,    _______, \
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,         KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______, \
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,        KC_NUHS, KC_NUBS, _______, _______, _______, \
-  _______, _______, _______, _______, _______, MT(MOD_LSFT, KC_BSPC),  _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+  _______, _______, _______, _______, _______, MT(MOD_LSFT, KC_BSPC),  _______, _______, _______, KC_VOLU, KC_MPLY \
 )
 };
 
