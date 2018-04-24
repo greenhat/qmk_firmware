d97d004da2908cca7c553e14d94e38f1b8391161
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2018-04-24 11:03:35 +0300

    Adds percent and hash in raised;

--- keyboards/jj40/keymaps/greenhat/keymap.c
+++ keyboards/jj40/keymaps/greenhat/keymap.c
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Ctrl |  -   |  =   |  [   |  ]   |  \   | Left | Down |  Up  | Right|   *  | Ctrl |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Esc  |      |  +   |  {   |  }   |   ^  |   $  |   (  |   )  |      |      |Enter |
+ * | Esc  |      |  +   |  {   |  }   |   ^  |   $  |   (  |   )  |   %  |   #  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      | Bksp(Shift) |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = KEYMAP( \
   KC_GRV,  KC_1,     KC_2,     KC_3,        KC_4,        KC_5,       KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    _______, \
   _______, KC_MINUS, KC_EQUAL, KC_LBRACKET, KC_RBRACKET, KC_BSLASH,  KC_LEFT,   KC_DOWN, KC_UP,  KC_RIGHT, KC_ASTR, _______, \
-  _______, _______,  KC_PLUS,  KC_LCBR,     KC_RCBR,     KC_CIRC,    KC_DLR,    KC_LPRN, KC_RPRN, _______, _______, _______, \
+  _______, _______,  KC_PLUS,  KC_LCBR,     KC_RCBR,     KC_CIRC,    KC_DLR,    KC_LPRN, KC_RPRN, KC_PERC, KC_HASH, _______, \
   _______, _______,  _______,  _______,     _______,     MT(MOD_LSFT, KC_BSPC), _______, _______, _______, _______, _______ \
 )
 };
