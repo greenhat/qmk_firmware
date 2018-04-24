d8610df89c16203a0cf9c41806a98ebbe9bb8da6
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2018-04-24 12:16:29 +0300

    comments;

--- keyboards/jj40/keymaps/greenhat/keymap.c
+++ keyboards/jj40/keymaps/greenhat/keymap.c
@@ -20,10 +20,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Esc  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Lower|  Alt |"(GUI)|DE(RA)| Space(Shift)|BS(RA)|?(Alt)|  GUI | Lower|      |
+ * |      | Lower|  Alt | GUI  |DE(RA)| Space(Shift)|BS(RA)|  Alt |  GUI | Lower|      |
  * `-----------------------------------------------------------------------------------'
  */
-  // todo ",? on modifiers?
 [_QWERTY] = KEYMAP( \
   KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,               KC_T,    KC_Y,        KC_U,                KC_I,    KC_O,    KC_P,      KC_QUOT, \
   KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,               KC_G,    KC_H,        KC_J,                KC_K,    KC_L,    KC_SCLN,   KC_RCTL, \
