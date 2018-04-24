4a5edc6de3110e0ca1ceb13ba5da2a433fac115e
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2018-04-24 12:20:33 +0300

    Adds | in raised;

--- keyboards/jj40/keymaps/greenhat/keymap.c
+++ keyboards/jj40/keymaps/greenhat/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |      |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  |   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Ctrl |  -   |  =   |  [   |  ]   |  \   | Left | Down |  Up  | Right|   *  | Ctrl |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = KEYMAP( \
-  KC_GRV,  KC_1,     KC_2,     KC_3,        KC_4,        KC_5,       KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    _______, \
+  KC_GRV,  KC_1,     KC_2,     KC_3,        KC_4,        KC_5,       KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    KC_PIPE, \
   _______, KC_MINUS, KC_EQUAL, KC_LBRACKET, KC_RBRACKET, KC_BSLASH,  KC_LEFT,   KC_DOWN, KC_UP,  KC_RIGHT, KC_ASTR, _______, \
   _______, KC_EXLM,  KC_PLUS,  KC_LCBR,     KC_RCBR,     KC_CIRC,    KC_DLR,    KC_LPRN, KC_RPRN, KC_PERC, KC_HASH, _______, \
   _______, _______,  _______,  _______,     _______,     MT(MOD_LSFT, KC_BSPC), _______, _______, _______, _______, _______ \
