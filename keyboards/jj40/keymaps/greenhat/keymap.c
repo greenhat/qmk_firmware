4781bbdd487a27b62c4e820a496d5d1762f18902
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2018-04-24 11:13:34 +0300

    Adds ! on raised; formatting;

--- keyboards/jj40/keymaps/greenhat/keymap.c
+++ keyboards/jj40/keymaps/greenhat/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
   // todo swap Alt and GUI
-  // todo " on left Raise?
+  // todo ",? on modifiers?
 [_QWERTY] = KEYMAP( \
   KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,       KC_T,    KC_Y,         KC_U,       KC_I,    KC_O,    KC_P,      KC_QUOT, \
   KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,       KC_G,    KC_H,         KC_J,       KC_K,    KC_L,    KC_SCLN,   KC_RCTL, \
@@ -56,15 +56,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Ctrl |  -   |  =   |  [   |  ]   |  \   | Left | Down |  Up  | Right|   *  | Ctrl |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Esc  |      |  +   |  {   |  }   |   ^  |   $  |   (  |   )  |   %  |   #  |Enter |
+ * | Esc  |  !   |  +   |  {   |  }   |   ^  |   $  |   (  |   )  |   %  |   #  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      | Bksp(Shift) |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
+  // todo restore tilde
 [_RAISE] = KEYMAP( \
   KC_GRV,  KC_1,     KC_2,     KC_3,        KC_4,        KC_5,       KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    _______, \
   _______, KC_MINUS, KC_EQUAL, KC_LBRACKET, KC_RBRACKET, KC_BSLASH,  KC_LEFT,   KC_DOWN, KC_UP,  KC_RIGHT, KC_ASTR, _______, \
-  _______, _______,  KC_PLUS,  KC_LCBR,     KC_RCBR,     KC_CIRC,    KC_DLR,    KC_LPRN, KC_RPRN, KC_PERC, KC_HASH, _______, \
+  _______, KC_EXLM,  KC_PLUS,  KC_LCBR,     KC_RCBR,     KC_CIRC,    KC_DLR,    KC_LPRN, KC_RPRN, KC_PERC, KC_HASH, _______, \
   _______, _______,  _______,  _______,     _______,     MT(MOD_LSFT, KC_BSPC), _______, _______, _______, _______, _______ \
 )
 };
