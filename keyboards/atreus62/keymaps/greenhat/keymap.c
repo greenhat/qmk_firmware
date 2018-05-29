016f5377ad09b0ba6a4d7e87e3b0f0cc396b0064
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2018-05-29 07:16:55 +0300

    esc to left raise;

--- keyboards/atreus62/keymaps/greenhat/keymap.c
+++ keyboards/atreus62/keymaps/greenhat/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT ,
     KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RCTL ,
     KC_ESC,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    KC_LCTL,  KC_LGUI, KC_LALT, KC_LALT, KC_LGUI, MO(_NAV), MT(MOD_LSFT, KC_SPC), MT(MOD_RSFT, KC_BSPC),  MO(_NAV), KC_RALT, KC_RGUI, KC_MINS, KC_QUOT, KC_ENT
+    KC_LCTL,  KC_LGUI, KC_LALT, KC_LALT, KC_LGUI, KC_ESC, MT(MOD_LSFT, KC_SPC), MT(MOD_RSFT, KC_BSPC),  MO(_NAV), KC_RALT, KC_RGUI, KC_MINS, KC_QUOT, KC_ENT
   ),
 
   [_NAV] = LAYOUT(
