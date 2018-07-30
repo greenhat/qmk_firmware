099257d0293b307475f2a040bd8675ab430d4d68
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2018-07-30 10:04:28 +0300

    utilize raised left home row;

--- keyboards/atreus62/keymaps/greenhat/keymap.c
+++ keyboards/atreus62/keymaps/greenhat/keymap.c
@@ -14,18 +14,19 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_DEFAULT] = LAYOUT( /* qwerty */
-  KC_MINUS,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQUAL,
+   KC_LBRC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_RBRC,
     KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT ,
-    KC_LCTL,   KC_A,    KC_S,    KC_D, LT(_LOW, KC_F), KC_G,                    KC_H, LT(_LOW, KC_J),KC_K, KC_L,    KC_SCLN, KC_RCTL ,
-    KC_BSLASH, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
+    /* KC_LCTL,   KC_A,    KC_S,    KC_D, LT(_LOW, KC_F), KC_G,                    KC_H, LT(_LOW, KC_J),KC_K, KC_L,    KC_SCLN, KC_RCTL , */
+    KC_LCTL,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RCTL ,
+    KC_EQUAL,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
     KC_LCTL, KC_NO, KC_GRV, KC_LBRACKET, KC_LGUI, KC_ESC, MT(MOD_LSFT, KC_SPC), MT(MOD_RSFT, KC_BSPC),  MO(_NAV), KC_RALT, KC_RBRACKET, KC_GRV, KC_NO, KC_ENT
   ),
 
   [_NAV] = LAYOUT(
     _______,     KC_F1,    KC_F2,       KC_F3,       KC_F4,     KC_F5,               KC_F6,     KC_F7,   KC_F8,    KC_F9,  KC_F10, KC_F11 ,
-     KC_GRV,   _______,  _______,     _______,     _______,   _______,               _______, _______, _______,  _______, _______, KC_PIPE,
-    _______,  KC_MINUS, KC_EQUAL, KC_LBRACKET, KC_RBRACKET, KC_BSLASH,               KC_LEFT, KC_DOWN,   KC_UP, KC_RIGHT, KC_ASTR, _______, 
-    _______,  KC_EXLM,  KC_PLUS,     KC_LCBR,     KC_RCBR,   KC_CIRC,                KC_DLR, KC_LPRN, KC_RPRN,  KC_PERC, KC_HASH, _______, 
+   KC_TILDE, _______,  _______,     _______,     _______,   _______,               _______, _______, _______,  _______, _______,   _______,
+    _______, KC_GRAVE, KC_BSLASH,    KC_UNDS,    KC_MINUS,   _______,               KC_LEFT, KC_DOWN,   KC_UP, KC_RIGHT, _______, _______, 
+    _______,  _______,  _______,     _______,     _______,   _______,                _______, _______, _______,  _______, _______, _______, 
     KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
