1e9ba09b36064ebe9210b0b77720a97e2d6a9644
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2020-06-20 15:41:02 +0300

    map Raise to rCtl (Delete); map Alt to Raise; map old Ctrls to Alts;

--- keyboards/atreus62/keymaps/greenhat/keymap.c
+++ keyboards/atreus62/keymaps/greenhat/keymap.c
@@ -16,9 +16,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_LBRC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_RBRC,
     KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT ,
     /* KC_LCTL,   KC_A,    KC_S,    KC_D, LT(_LOW, KC_F), KC_G,                    KC_H, LT(_LOW, KC_J),KC_K, KC_L,    KC_SCLN, KC_RCTL , */
-    KC_LCTL,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RCTL ,
+    KC_LALT,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RALT ,
     KC_EQUAL,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    KC_LCTL, KC_NO, KC_GRV, KC_LBRACKET, KC_LGUI, MT(MOD_LCTL, KC_ESC), MT(MOD_LSFT, KC_SPC),       MT(MOD_RSFT, KC_BSPC),  MO(_NAV), KC_RALT, KC_RBRACKET, KC_GRV, KC_NO, KC_ENT
+    KC_LCTL, KC_NO, KC_GRV, KC_LBRACKET, KC_LGUI, MT(MOD_LCTL, KC_ESC), MT(MOD_LSFT, KC_SPC),       MT(MOD_RSFT, KC_BSPC),  MT(MOD_RCTL, KC_DELETE), MO(_NAV), KC_RBRACKET, KC_GRV, KC_NO, KC_ENT
   ),
 
   [_NAV] = LAYOUT(
